ae12e2d5956995a4678d35d74da0dd56de6540dc
Mohadeb Kumar <78855568+Mohadeb-Kumar@users.noreply.github.com>
2021-02-25 13:50:13 +0600

    Add files via upload

---
+++ Hello World.cpp
@@ -0,0 +1,10 @@
+//Mohadeb Kumar (CSE)
+//Hajee Mohammad Danesh Science & Technology University, Dinajpur
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "Hello World!"<<endl;
+    return 0;
+}
